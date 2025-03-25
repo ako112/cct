91715b5cb7aef96daac1de9d3657694265c8693a
ako112 <140101045+ako112@users.noreply.github.com>
2025-03-25 13:47:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ source_urls = [
     "https://live.zbds.top/tv/iptv4.txt",
     "https://ghproxy.net/https://raw.githubusercontent.com/ssili126/tv/main/itvlist.m3u",
    "https://raw.githubusercontent.com/fanmingming/live/refs/heads/main/tv/m3u/ipv6.m3u",
-    "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt"
+    "https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
+   "https://raw.githubusercontent.com/Guovin/iptv-api/gd/output/result.txt"
    ######################################################################################################  
    
     #"https://github.com/SPX372928/MyIPTV/blob/master/%E9%BB%91%E9%BE%99%E6%B1%9FPLTV%E7%A7%BB%E5%8A%A8CDN%E7%89%88.txt"
