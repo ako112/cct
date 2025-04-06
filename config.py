9247e4e5e66530c6cfb1118d3b689191d6547ae1
ako112 <140101045+ako112@users.noreply.github.com>
2025-04-06 15:26:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -24,23 +24,12 @@ epg_urls = [
 
 # URL 黑名单：包含需要过滤的 URL 关键词
 url_blacklist = [
-    "some_unwanted_keyword",
-    "another_unwanted_keyword",
+    "cdn.iptv8k.top",
+"tv.nezha.su:35455",
     # 可以添加更多关键词
 ]
 
 # IP 版本优先级： "ipv6" 或 "ipv4"
 ip_version_priority = "ipv4"  # 设置默认优先级
 
-# 公告信息（可选）：在生成的 M3U 和 TXT 文件开头添加公告
-announcements = [
-    {
-        'channel': '公告',
-        'entries': [
-            {'name': '欢迎使用！', 'url': '#', 'logo': ''},
-            {'name': '更多信息请访问...', 'url': 'your_website_here', 'logo': ''},
-            # 可以添加更多公告条目
-        ]
-    },
-    # 可以添加更多公告分组
-]
+
